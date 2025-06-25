3f22e67cf4cd75958057108df354e5c55b856127
M0RP43U588 <lukasthommen06@gmail.com>
2025-06-25 20:51:41 +0200

    what the column

--- Roulette.java
+++ Roulette.java
@@ -43,9 +43,6 @@ public class Roulette {
   static final int[] FIRSTTWELVE         = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
   static final int[] SECONDTWELVE        = { 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
   static final int[] THIRDTWELVE         = { 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };
-  static final int[] FIRSTCOL            = { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34 };
-  static final int[] SECONDCOL           = { 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35 };
-  static final int[] THIRDCOL            = { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36 };
   static final int[] ROWONE              = { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34 };
   static final int[] ROWTWO              = { 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35 };
   static final int[] ROWTHREE            = { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36 };
